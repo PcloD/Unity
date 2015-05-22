5844c8a2c16c5e229f8de219b28458d369e0afb6
Ben Sherratt <ben@vitei.com>
2015-05-22 13:26:12 +0900

    Initial commit

---
+++ EditorUtilities/Scripts/CrossSceneLink.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CrossSceneLink : MonoBehaviour
+{
+	public string componentName;
+	public string attributeName;
+	public string gameObjectPath;
+
+	string _componentName;
+	string _attributeName;
+	string _gameObjectPath;
+
+	void Update()
+	{
+		if(componentName != _componentName || attributeName != _attributeName || gameObjectPath != _gameObjectPath)
+		{
+			Component component;
+			System.Reflection.FieldInfo info;
+			GameObject linkedGameObject;
+
+			_componentName = componentName;
+			_attributeName = attributeName;
+			_gameObjectPath = gameObjectPath;
+
+			component = gameObject.GetComponent(componentName);
+
+			if(component == null)
+			{
+				return;
+			}
+
+			info = component.GetType().GetField(attributeName);
+
+			if(info == null)
+			{
+				return;
+			}
+
+			linkedGameObject = GameObject.Find(gameObjectPath);
+			info.SetValue(component, linkedGameObject);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Vitei's Unity Utilities
+
+Folder Structure: UtilityUse/[Scripts/Editor etc.]/*
+
+## Current Utilities:
+
+EditorUtilities/Scripts/CrossSceneLink.cs - Link a component's field value to a game object saved in another scene
+
